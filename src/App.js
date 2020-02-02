b40a5d33fef164c9fb1eb6830de533313a71e8f8
zandercs <46501347+zandercs@users.noreply.github.com>
2020-02-02 14:03:03 -0500

    Better async

--- src/App.js
+++ src/App.js
@@ -2,76 +2,67 @@ import React,{ Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  // function getSrd(url){
-  //     return new Promise((resolve, reject) => {
-  //         $.ajax({
-  //             url: 'http://www.dnd5eapi.co'+url,
-  //             dataType: 'json',
-  //             context: document.body,
-  //             success: function(data) {
-  //                 //called when successful
-  //                 console.log(data);
-  //                 // return data;
-  //                 return resolve(data);
-  //             },
-  //             error: function(e) {
-  //                 //called when there is an error
-  //                 console.log(e.message);
-  //                 // return e;
-  //                 return reject(e.message);
-  //             }
-  //         });
-  //     })
-  //
-  // }
   constructor(props){
     super(props);
 
     this.state = {
-      monsterList: [],
-      spellsList: [],
-      poop: [
-        {index:"words 0"},
-        {index:"words 1"},
-        {index:"words 2"},
-        {index:"words 3"}
-      ]
     };
   }
 
   componentDidMount() {
-    let monsterList = 'http://www.dnd5eapi.co/api/monsters';
-    fetch(monsterList)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log('result-items: '+JSON.stringify(result));
-          this.setState({monsterList: result.results});
-        },
-        (error) => {
-          console.log('result-items:'+error)
-        }
-      )
-
-      let spellsList = 'http://www.dnd5eapi.co/api/spells';
-      fetch(spellsList)
+    let apiCalls = {
+      'monsters':'monsters',
+      'spells':'spells'
+    };
+    for (const apiCall in apiCalls) {
+      console.log(`${apiCall} : ${apiCalls[apiCall]}`);
+      fetch(`http://www.dnd5eapi.co/api/${apiCall}`)
         .then(res => res.json())
         .then(
           (result) => {
             console.log('result-items: '+JSON.stringify(result));
-            this.setState({spellsList: result});
+            this.setState({[`${apiCall}List`]: result.results,[`${apiCall}Ready`]:true});
           },
           (error) => {
             console.log('result-items:'+error)
           }
         )
+    }
+
+    // let monsterList = 'http://www.dnd5eapi.co/api/monsters';
+    // fetch(monsterList)
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       console.log('result-items: '+JSON.stringify(result));
+    //       this.setState({monsterList: result.results});
+    //     },
+    //     (error) => {
+    //       console.log('result-items:'+error)
+    //     }
+    //   )
+    //
+    //   let spellsList = 'http://www.dnd5eapi.co/api/spells';
+    //   fetch(spellsList)
+    //     .then(res => res.json())
+    //     .then(
+    //       (result) => {
+    //         console.log('result-items: '+JSON.stringify(result));
+    //         this.setState({spellsList: result});
+    //       },
+    //       (error) => {
+    //         console.log('result-items:'+error)
+    //       }
+    //     )
   }
   render(){
     return (
       <div className="App">
-        <select>
-        {this.state.monsterList.map((monster, index)=>{
-          return <option value={monster.index} key={index}>{monster.index}</option>
+        <label>Monster: </label>
+        <select name="selectedMonster">
+          <option value="false">Choose a monster...</option>
+        {this.state && this.state.monstersList && this.state.monstersList.map((monster, index)=>{
+          return <option value={monster.url} key={index}>{monster.name}</option>
         })}
         </select>
       </div>
